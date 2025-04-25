37f5d82923a6ebfe4a40560d0f5475852860e1f8
ao <aolcq8@gmail.com>
2025-04-26 01:20:01 +0900

    Add mdx setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: "https://braversh.github.io",
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()],
   markdown: {
     shikiConfig: {
       themes: {
